aece7e1b316fd201ed9dae5cb852016b352a9454
Marcelo Bittencourt <tchelopoa@gmail.com>
2021-11-27 11:40:32 -0300

    Adiciona o primeiro teste

--- src/test/java/steps/VisualizarClientesSteps.java
+++ src/test/java/steps/VisualizarClientesSteps.java
@@ -1,25 +1,51 @@
 package steps;
 
+import io.cucumber.java.After;
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Então;
 import io.cucumber.java.pt.Quando;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class VisualizarClientesSteps {
 
+    private static WebDriver driver;
+
     @Dado("que estou na pagina inicial")
     public void que_estou_na_pagina_inicial() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        driver = new ChromeDriver();
+        //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://localhost:3000/clients");
     }
     @Quando("não estou logado")
     public void não_estou_logado() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
     @Então("visualizo a cliente {string} com e-mail {string} na posição {string}")
-    public void visualizo_a_cliente_com_e_mail_na_posição(String string, String string2, String string3) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void visualizo_a_cliente_com_e_mail_na_posição(String string, String string2, String posicaoString) {
+        System.out.println(posicaoString);
+        int posicao = Integer.parseInt(posicaoString);
+        String nomeEsperado = string;
+        String emailEsperado = string2;
+
+        WebElement elementoNome = driver.findElement(By.cssSelector("#root > div > div > table > tbody > tr:nth-child(" + posicaoString + ") > td:nth-child(1)"));
+        String nomeEncontrado = elementoNome.getText();
+
+        WebElement elementoEmail = driver.findElement(By.cssSelector("#root > div > div > table > tbody > tr:nth-child(" + posicaoString + ") > td:nth-child(2)"));
+        String emailEncontrado = elementoEmail.getText();
+
+        assertThat(nomeEncontrado).isEqualTo(nomeEsperado);
+
+        assertThat(emailEncontrado).isEqualTo(emailEsperado);
+    }
+
+    @After
+    public void finalizar() {
+        driver.quit();
     }
 
 
